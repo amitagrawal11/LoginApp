b10ecb20754337b5b45f691d529cf0b08c423ddb
amitagrawal11 <amit.agrawal11@hotmail.com>
2016-12-21 04:16:07 +0530

    applying regEx for password field

--- LoginApp.js
+++ LoginApp.js
@@ -47,13 +47,18 @@ app.service('AuthService', function(EmployeeFactory, $timeout) {
     	$timeout(function() {                
             var response;                
             EmployeeFactory.getEmpByUserId(username).then(function(response) {
-            	if(response.employee && response.employee.password === password){
-            		response.success = true;
-            	} else if(response.employee && response.employee.password !== password){
-            		response.success = false;
-            		response.message = "Invalid employee credentials, Try again!";
+            	if(!/[^a-zA-Z0-9 ]/.test(password)){
+            		if(response.employee && response.employee.password === password){
+	            		response.success = true;
+	            	} else if(response.employee && response.employee.password !== password){
+	            		response.success = false;
+	            		response.message = "Invalid employee credentials, Try again!";
+	            	} else {
+	            		response.success = false;
+	            	}
             	} else {
             		response.success = false;
+            		response.message = "Password contains special characters!";
             	}
                 callback(response);                     
             }, function(error){
