c48f816f5c94add396e177333fcc2c0d8c041aea
Amit Priyadarshi Agrawal <amit.agrawal11@hotmail.com>
2017-01-03 18:36:29 +0530

    Adding bootstrap and routeProviders

--- LoginApp.js
+++ LoginApp.js
@@ -1,8 +1,46 @@
 /* Angular Login Application */
 
-var app = angular.module("EmpLoginApp", []);
+var app = angular.module("EmpLoginApp", ['ngRoute']);
 
-app.controller('EmployeeCtrl', ['$scope', 'AuthService', function($scope, AuthService) {
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/login", {
+            controller: "LoginCtrl",
+            templateUrl: "views/login.html",
+            hideMenus: true
+        })
+
+        .when("/register", {
+            controller: "RegisterCtrl",
+            templateUrl: "views/register.html",
+            hideMenus: true
+        })
+
+        .when("/home", {
+            controller: "HomeCtrl",
+            templateUrl: "views/home.html",
+            hideMenus: true
+        })
+
+        .otherwise({ redirectTo: "/login" });
+});
+
+/*.run(function ($rootScope, $location, $cookieStore, $http) {
+    // keep user logged in after page refresh
+    $rootScope.globals = $cookieStore.get('globals') || {};
+    if ($rootScope.globals.currentUser) {
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+    }
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        // redirect to login page if not logged in
+        if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+            $location.path('/login');
+        }
+    });
+});*/
+
+app.controller('LoginCtrl', function($scope, AuthService, $location) {
     $scope.emp = {};
     $scope.emp.username = "";
     $scope.emp.password = "";
@@ -13,9 +51,11 @@ app.controller('EmployeeCtrl', ['$scope', 'AuthService', function($scope, AuthSe
     $scope.doSignIn = function() {
         AuthService.loginService($scope.emp.username, $scope.emp.password, function(response){
         	if(response.success){
-        		$scope.showHomepage = true;
+                //redirect it to home page
+                $location.path("/home");
+        		/*$scope.showHomepage = true;
         		$scope.successMsg = "User has signed-in successfully!";
-        		$scope.errorMsg = "";
+        		$scope.errorMsg = "";*/
         	} else {
         		$scope.showHomepage = false;
         		$scope.successMsg = "";
@@ -30,4 +70,9 @@ app.controller('EmployeeCtrl', ['$scope', 'AuthService', function($scope, AuthSe
         }
         return false;
     }
+});
+
+
+app.controller('HomeCtrl', ['$scope', 'AuthService', function($scope, AuthService) {
+    $scope.displaySuccessMsg = "You have successfully logged in";
 }]);
