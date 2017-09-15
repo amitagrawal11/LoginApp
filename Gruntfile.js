f8099a7cb09f8e750372b867e0d5f9531ee839ae
Amit Agrawal <amit.agrawal11@hotmail.com>
2017-09-16 02:33:54 +0530

    updating working grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,68 @@
- var path = require('path');
- //add the config file name
- var configFile = path.resolve('./config.json');
- var config = require(configFile);
- module.exports = function(grunt) {
-     grunt.initConfig({
-         msdeploy: {
-             push: {
-                 options: {
-                     verb: 'sync',
-                     allowUntrusted: true,
-                     source: {
-                         'contentPath': config.srcPath
-                     },
-                     dest: {
-                         contentPath: config.contentPath,
-                         wmsvc: config.serverAddress,
-                         userName: config.userName,
-                         authType: 'basic'
-                             //password: config.password
-                     }
-                 }
-             }
-         }
-     });
-     grunt.loadNpmTasks('grunt-msdeploy');
-     //add task for deployment - copying the dist from local server to remote server
-     grunt.registerTask('deploy', ['msdeploy:push']);
- }
+module.exports = function(grunt) {
+
+	//-- config main project setting
+    grunt.initConfig({
+    	
+    	//-- basic setting
+        //-- read dependencied from 'package.json' 
+    	pkg : grunt.file.readJSON('package.json'), 
+
+        //-- clean build folder before copy file from cwd
+        clean: {
+            build: {
+                src: [ 'build' ]
+            }
+        },
+        
+        //-- copy all files from curront working directory to build directory        
+        copy: {
+            files: {
+                cwd: 'app',  // set current working directory
+                src: '**/*',          // copy all files and subfolders
+                dest: 'build',        // destination folder
+                expand: true          // required when using cwd
+            }
+        },
+
+        //-- uglify all js files in app folder
+        uglify: {
+            options: {
+                mangle: false
+                //preserveComments :true
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'build',
+                    src: ['**/*.js'],
+                    dest: 'build'
+                }]
+            }
+        },
+        //-- css minification
+        // cssmin : {
+        //     combine : {
+        //         files : {
+        //             'build/assets/stylesheets/main.min.css' : [
+        //                 'build/app/assets/stylesheets/animate.css',
+        //                 'build/app/assets/stylesheets/jquery.window.css',
+        //                 'build/app/assets/stylesheets/r7.base.css',
+        //                 'build/app/assets/stylesheets/r7.custom-gis.css',
+        //                 'build/app/assets/stylesheets/r7.modules.css',
+        //                 'build/app/assets/stylesheets/style.css'
+        //             ]
+        //         }
+        //     }
+        // },
+    });
+
+    //-- load the plugins
+    //grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Default task(s).
+	grunt.registerTask('default', ['clean','copy', 'uglify']);
+	
+};
